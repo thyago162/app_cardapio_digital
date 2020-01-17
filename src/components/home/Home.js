4c12696b4ffe948ddb9edfaacf7cc8a1fc57a349
thyago162 <thyago162@gmail.com>
2020-01-17 18:56:21 -0300

    Finalizando o home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,19 +1,72 @@
 import React, {Component} from 'react';
-import {View,Text, Button} from 'react-native';
+import {View,Text, Button, StyleSheet, Picker, Image} from 'react-native';
 
 export default class HomeScreen extends Component {
 
     static navigationOptions = {
-        title: 'Bem vindo'
+        title: 'Cardápio digital',
+        headerTintColor: 'white',
+        headerStyle: {
+            backgroundColor: '#333333',
+        },
     }
 
     render() {
         const {navigate} = this.props.navigation
         return(
-            <View>
-                <Text>This is my home</Text>
-                <Button title="Vamos navegar" onPress={() => navigate('About')}/>
+            <View style={styles.container}>
+                <View style={styles.logo}>
+                    <Image source={require('../../assets/images/logo.png')} style={styles.logo_image}/>
+                </View>
+
+                <View style={styles.table}>
+                    <Picker>
+                        <Picker.Item  label="Escolha a mesa" value=''/>
+                        <Picker.Item label="Mesa 01" value='1'/>
+                    </Picker>
+                </View>
+
+                <View style={styles.btn_comanda}>
+                    <Button title="Gerar comanda" onPress={() => navigate('About')} color="#FF7F00"/>
+                </View>
+                
+
+                
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'column'
+    },
+
+    logo: {
+        alignContent: 'center',
+        alignItems: 'center',
+        marginTop: 10,
+        height: 250,
+    },
+
+    logo_image: {
+        marginTop: 30,
+        width: 300,
+        height: 210
+    },
+
+    table: {
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginLeft: 15,
+        marginRight: 15,
+        marginTop: 60
+    },
+
+    btn_comanda: {
+        marginLeft: 15,
+        marginRight: 15,
+        marginTop: 30,
+    }
+
+})
